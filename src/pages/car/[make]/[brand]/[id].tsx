41d64223536fea0a21b3d08b937a480b520fa7ef
CURVX <chakrawarti.anupam@gmail.com>
2021-05-17 19:22:41 +0530

    Completed assignment

---
+++ src/pages/car/[make]/[brand]/[id].tsx
@@ -0,0 +1,106 @@
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import { GetServerSideProps } from "next";
+import React, { useState } from "react";
+import { CarModel } from "../../../../../api/Car";
+import { openDB } from "../../../../openDB";
+import Head from "next/head";
+
+import { Skeleton } from "@material-ui/lab";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+  },
+  img: {
+    width: "100%",
+  },
+}));
+
+interface CarDetailsProps {
+  car: CarModel | null | undefined;
+}
+
+export default function CarDetails({ car }: CarDetailsProps) {
+  const classes = useStyles();
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 3000);
+
+  if (!car) {
+    return <h1>Sorry, car not found!</h1>;
+  }
+
+  if (isLoading) {
+    return <Skeleton variant="rect" width={800} height={450} />;
+  }
+
+  if (!isLoading) {
+    return (
+      <div>
+        {/* For SEO */}
+        <Head>
+          <title>{`${car.make}-${car.model}`}</title>
+        </Head>
+        <Paper className={classes.paper}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={5}>
+              <img className={classes.img} alt="complex" src={car.photoUrl} />
+            </Grid>
+            <Grid item xs={12} sm={6} md={7} container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                  <Typography variant="h5">
+                    {`${car.make} ${car.model}`}
+                  </Typography>
+                  <Typography gutterBottom variant="h4">
+                    ${car.price}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="body2"
+                    color="textSecondary">
+                    <b>Year:</b> {car.year}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="body2"
+                    color="textSecondary">
+                    <b>KMs:</b> {car.kilometers}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="body2"
+                    color="textSecondary">
+                    <b>Fuel Type:</b> {car.fuelType}
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    variant="body1"
+                    color="textSecondary">
+                    <b>Details:</b> {car.details}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const id = ctx.params.id;
+  const db = await openDB();
+  const car = await db.get<CarModel | undefined>(
+    "SELECT * FROM Car where id = ?",
+    id
+  );
+  return { props: { car: car || null } };
+};
